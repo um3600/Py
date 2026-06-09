bf2561421fc35bf9710925b3c12a4d55089ee5d7
U M A R - B I N - R I A Z <umar.bin.riaz99@gmail.com>
2026-06-09 22:37:16 +0500

    Create File

---
+++ assignment4.py
@@ -0,0 +1,74 @@
+a = 10
+b = 5
+
+if a > b:
+    print("a is greater than b")
+
+    name1 = "Ali"
+    name2 = "Ali"
+
+    if name1 == name2:
+        print("Both names matched")
+
+        first_number = 4
+        second_number = 8
+
+        if first_number < second_number:
+            print("First number is less than second number")
+
+            age = 18
+
+            if age >= 18:
+                print("You can vote")
+
+                marks = 65
+
+                if marks >= 50:
+                    print("Pass")
+                    else:
+                        print("Fail")
+
+                        num1 = 12
+                        num2 = 7
+
+                        if num1 > num2:
+                            print(num1, "is greater")
+                            else:
+                                print(num2, "is greater")
+
+                                text = ""
+
+                                if text:
+                                    print("String is not empty")
+                                    else:
+                                        print("String is empty")
+
+                                        name = "Muhammad Riaz"
+
+                                        if name:
+                                            print("Name is available")
+
+                                            value = 0
+
+                                            if value:
+                                                print("Truthy value")
+                                                else:
+                                                    print("Falsy value")
+
+                                                    value2 = 10
+
+                                                    if value2:
+                                                        print("Truthy value")
+                                                        else:
+                                                            print("Falsy value")
+
+                                                            student_marks = 85
+
+                                                            if student_marks >= 80:
+                                                                print("A Grade")
+                                                                elif student_marks >= 60:
+                                                                    print("B Grade")
+                                                                    elif student_marks >= 50:
+                                                                        print("C Grade")
+                                                                        else:
+                                                                            print("Fail")
